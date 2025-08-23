09712a51f9806a5d000ac56bc6a3cf0d68c96885
Andrew Dabrowski <andrewfdabrowski@gmail.com>
2025-08-22 23:16:44 -0400

    fix: naming convention

--- src/analytics_eda/core/data_quality/__init__.py
+++ src/analytics_eda/core/data_quality/__init__.py
@@ -1,3 +1,3 @@
 from .string_coercion_bar_plot import StringCoercionBarPlot, StringCoercionBarContext
-from .categorical_cleanliness_bar import CategoricalCleanlinessBarPlot, CategoricalCleanlinessBarContext
+from .categorical_cleanliness_bar_plot import CategoricalCleanlinessBarPlot, CategoricalCleanlinessBarContext
 from .missing_data_bar_plot import MissingDataBarContext, MissingDataBarPlot
